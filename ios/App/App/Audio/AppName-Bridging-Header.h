203ea6df91806d1f8a3917bc3c391d2749fbf6cc
Stnaire <Stnaire@users.noreply.github.com>
2024-05-30 10:24:22 +0200

    add ios files

---
+++ ios/App/App/Audio/AppName-Bridging-Header.h
@@ -0,0 +1,6 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+// I don't remember  what this file was for.
+//
+// Replace "AppName" in the file name with your app name.
